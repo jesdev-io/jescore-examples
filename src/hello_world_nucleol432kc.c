b3d54b7ac1a2b2fd2d561d50ac75956e9ed95be2
jake-is-ESD-protected <j.tschavoll@tu-berlin.de>
2025-10-01 16:51:32 +0200

    hello world example

---
+++ src/hello_world_nucleol432kc.c
@@ -0,0 +1,86 @@
+#include <jescore.h>
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+void Error_Handler();
+
+// definitions for a Nucleo-L432kc
+#define LED_PORT GPIOB
+#define LED_GPIO GPIO_PIN_3
+#define LED_ENABLE_PORT() __HAL_RCC_GPIOB_CLK_ENABLE()
+
+void blink(void* p){
+    static uint8_t act = 0;
+    act = !act;
+    while(act){
+        HAL_GPIO_TogglePin(LED_PORT, LED_GPIO);
+        jes_delay_job_ms(1000);
+    }
+}
+
+void hello(void* p){
+    uart_unif_writef("Hello World!\n\r");
+}
+
+int main(void){
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+
+    jes_init();
+    jes_register_and_launch_job("blink", 256, 1, blink, 1);
+    jes_register_and_launch_job("hello", 256, 1, hello, 0);
+    jes_dispatch();
+}
+
+void SystemClock_Config(void){
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+    RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+    RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+    RCC_OscInitStruct.PLL.PLLM = 1;
+    RCC_OscInitStruct.PLL.PLLN = 40;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+    RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+    RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK){
+        Error_Handler();
+    }
+
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK){
+        Error_Handler();
+    }
+}
+
+static void MX_GPIO_Init(void){
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    LED_ENABLE_PORT();
+
+    GPIO_InitStruct.Pin = LED_GPIO;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
+
+    HAL_GPIO_WritePin(LED_PORT, LED_GPIO, GPIO_PIN_RESET);
+}
+
+void Error_Handler(void){
+  __disable_irq();
+  while (1) { }
+}
