831bc5624f5b8c63a4f9e7e664db62eaede5439b
jake-is-ESD-protected <j.tschavoll@tu-berlin.de>
2025-10-02 16:48:52 +0200

    more esp boards, examples from main repo

---
+++ src/fsm_cli_arduino.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include <jescore.h>
+
+#define LED_PIN LED_BUILTIN
+
+static bool is_on = false;
+static bool processing = false;
+
+void lights(void* p){
+    /* 
+    This function will be executed when the first word of your
+    CLI command is "lights", as registered in line 60 below. All
+    other words that follow, such as "on" or "off" will pe passed
+    as args to this job and can be retrieved as full string with
+    jes_job_get_args().
+    */
+    char* args = jes_job_get_args();
+    char* arg = strtok(args, " ");
+    if (!arg){
+        uart_unif_write("No argument specified! Use <on> or <off>\n\r");
+        return;
+    }
+    if (strcmp(arg, "on") == 0){
+        if(is_on) return; // nothing to do
+        if(processing) return; // blocked by lights_off
+        processing = true;
+        for(uint8_t i = 0; i < 255; i++){
+            analogWrite(LED_PIN, i);
+            jes_delay_job_ms(5);
+        }
+        is_on = true;
+        processing = false;
+    }
+    else if (strcmp(arg, "off") == 0){
+        if(!is_on) return; // nothing to do
+        if(processing) return; // blocked by lights_off
+        processing = true;
+        for(uint8_t i = 255; i > 0; i--){
+            analogWrite(LED_PIN, i);
+            jes_delay_job_ms(5);
+        }
+        is_on = false;
+        processing = false;
+    }
+    else if (strcmp(arg, "state") == 0){
+        uart_unif_writef("lights are %s\n\r",(is_on ? "on" : "off"));
+    }
+    else{
+        uart_unif_write("unknown argument!\n\r");
+    }
+}
+
+void setup() {
+    jes_init();
+    pinMode(LED_PIN, OUTPUT);
+    analogWrite(LED_PIN, 255);
+    jes_register_job("lights", 2048, 1, lights, false);
+}
+
+void loop() {
+    // nothing to do here!
+    // jescore is now purely controlled by the CLI
+    // call `jescore lights on` to see the LED fade on
+    // call `jescore lights status` to get a print
+    // of the current status of the LED FSM
+}
---
+++ src/sync_async_arduino.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <jescore.h>
+
+#define LED_PIN LED_BUILTIN
+
+static uint32_t blink_pause = 1000;
+
+void blink_forever(void* p){
+    /*
+    This function is a placeholder for a  "time critical" loop 
+    that does not have the capability of sampling user input. 
+    This is the "sync"-job that we want to steer with the "async"-
+    job. This job is an infinite, synchronous loop.
+    */
+    while(1){
+        digitalWrite(LED_PIN, 1);
+        jes_delay_job_ms(blink_pause);
+        digitalWrite(LED_PIN, 0);
+        jes_delay_job_ms(blink_pause);
+    }
+}
+
+void blink_switch(void* p){
+    /*
+    This function handles "async" user input and applies it to
+    the time critical job. In this very simple example, the shared
+    resource is a single uint32_t variable "blink_pause". This job
+    handles asynchronous user input and then exits.
+    */
+    char* arg = jes_job_arg_next();
+    if(!arg){
+        uart_unif_write("Usage: switch <time in ms>\r\n");
+        return;
+    }
+    while(arg != NULL){
+        blink_pause = atoi(arg);
+        uart_unif_writef("Blink rate set to %s ms\n\r", arg);
+        arg = jes_job_arg_next();
+    }
+}
+
+void setup() {
+    jes_init();
+    pinMode(LED_PIN, OUTPUT);
+    jes_register_job("switch", 2048, 1, blink_switch, false);
+    jes_register_and_launch_job("blink", 2048, 1, blink_forever, true);
+}
+
+void loop() {
+    // nothing to do here!
+    // call `jescore switch <time in ms>` to see the LED change blink speed
+}
